29552a584b61c037d6831958b27a512e4a439139
Luis Riveros <luis.riveros_ex@scotiabank.cl>
2019-10-22 20:53:22 -0300

    feat(init): initial spring project

---
+++ src/main/java/cl/previred/challenge/periods/MissingPeriodsApplication.java
@@ -0,0 +1,13 @@
+package cl.previred.challenge.periods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingPeriodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingPeriodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/previred/challenge/periods/MissingPeriodsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.previred.challenge.periods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissingPeriodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
