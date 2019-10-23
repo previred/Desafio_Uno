93e11587a7faa98be3662abf928011c5b0fa74ca
Luis Riveros <luis.riveros_ex@scotiabank.cl>
2019-10-23 00:35:32 -0300

    feat(nivel 3): Solucion nivel 3

--- src/main/java/cl/previred/challenge/periods/MissingPeriodsApplication.java
+++ src/main/java/cl/previred/challenge/periods/MissingPeriodsApplication.java
@@ -1,13 +1,32 @@
 package cl.previred.challenge.periods;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-@SpringBootApplication
+import java.time.format.DateTimeFormatter;
+
+@SpringBootApplication(scanBasePackages = "cl.previred.challenge.periods")
 public class MissingPeriodsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MissingPeriodsApplication.class, args);
-	}
+    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissingPeriodsApplication.class, args);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        builder.simpleDateFormat(DEFAULT_DATE_FORMAT);
+        builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT)));
+
+        return builder;
+    }
 
 }
