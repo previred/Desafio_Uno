65d464d8b465d6706d295796753aa181e3a74501
julillorojas <ju.lillo.rojas@gmail.com>
2019-12-11 14:34:31 -0300

    primer commit

---
+++ DatesRecovery/src/main/java/cl/julillo/datesrecovery/DatesRecoveryApplication.java
@@ -0,0 +1,13 @@
+package cl.julillo.datesrecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatesRecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatesRecoveryApplication.class, args);
+	}
+
+}
---
+++ DatesRecovery/src/test/java/cl/julillo/datesrecovery/DatesRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.julillo.datesrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatesRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
