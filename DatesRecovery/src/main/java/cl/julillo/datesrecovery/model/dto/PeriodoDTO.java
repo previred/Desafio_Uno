87bb95dd33e0844a98d07c3145d6d21a6f91e3a6
julillorojas <ju.lillo.rojas@gmail.com>
2019-12-12 11:24:16 -0300

    juan andres lillo rojas / ju.lillo.rojas@gmail.com / linkedin
    arreglos menor api swagger

--- DatesRecovery/src/main/java/cl/julillo/datesrecovery/model/dto/PeriodoDTO.java
+++ DatesRecovery/src/main/java/cl/julillo/datesrecovery/model/dto/PeriodoDTO.java
@@ -26,7 +26,7 @@ public class PeriodoDTO {
 	private LocalDate fechaFin;
 	@ApiModelProperty(example = "['1969-03-01', '1969-05-01', '1969-09-01', '1970-01-01']")
 	private List<LocalDate> fechas;
-	@ApiModelProperty(example = "['1969-04-01', '1969-06-01, '1969-07-01', '1969-08-01', '1969-10-01', '1969-11-01', '1969-12-01']")
+	@ApiModelProperty(example = "['1969-04-01', '1969-06-01', '1969-07-01', '1969-08-01', '1969-10-01', '1969-11-01', '1969-12-01']")
 	private List<LocalDate> fechasFaltantes;
 
 }
