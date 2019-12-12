d1dc1a2acb5b195413c9773f22e64fad556f81fc
julillorojas <ju.lillo.rojas@gmail.com>
2019-12-12 10:45:18 -0300

    done!

--- DatesRecovery/src/main/java/cl/julillo/datesrecovery/service/impl/RecuperarFechasServiceImpl.java
+++ DatesRecovery/src/main/java/cl/julillo/datesrecovery/service/impl/RecuperarFechasServiceImpl.java
@@ -1,6 +1,9 @@
 package cl.julillo.datesrecovery.service.impl;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -19,7 +22,21 @@ public class RecuperarFechasServiceImpl implements iRecuperarFechasService {
 
 	@Override
 	public PeriodoDTO recuperarFechas() {
-		return this.gddCli.getPeriodos(Collections.singletonMap(HttpHeaders.ACCEPT, MediaType.toString(Collections.singletonList(MediaType.APPLICATION_JSON))));
+		PeriodoDTO dto = this.gddCli.getPeriodos(Collections.singletonMap(HttpHeaders.ACCEPT, MediaType.toString(Collections.singletonList(MediaType.APPLICATION_JSON))));
+		dto.setFechasFaltantes(this.getfechasFaltantes(dto.getFechaCreacion(), dto.getFechaFin(), dto.getFechas()));
+		return dto;
+	}
+	
+	
+	private List<LocalDate> getfechasFaltantes(LocalDate fechaCreacion, LocalDate fechaFin, List<LocalDate> fechas) {
+		List<LocalDate> fechasFaltantes = new ArrayList<LocalDate>();
+		while(!fechaCreacion.isAfter(fechaFin)) {
+			if (!fechas.contains(fechaCreacion)) {
+				fechasFaltantes.add(fechaCreacion);
+			}
+			fechaCreacion = fechaCreacion.plusMonths(1L);
+		}
+		return fechasFaltantes;
 	}
 
 }
