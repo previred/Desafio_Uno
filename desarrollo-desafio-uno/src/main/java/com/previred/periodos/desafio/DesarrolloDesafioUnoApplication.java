300eb5677aba6e3a0c10d4d40f64fa8e123317fe
sati <jsatizabal@databi.com.co>
2020-03-23 20:45:25 -0300

    spring Initializr

---
+++ desarrollo-desafio-uno/src/main/java/com/previred/periodos/desafio/DesarrolloDesafioUnoApplication.java
@@ -0,0 +1,13 @@
+package com.previred.periodos.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloDesafioUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloDesafioUnoApplication.class, args);
+	}
+
+}
---
+++ desarrollo-desafio-uno/src/test/java/com/previred/periodos/desafio/DesarrolloDesafioUnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.previred.periodos.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloDesafioUnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
