9d706f23e5fe746c7516e2ac71a1db7fe572a05a
sati <jsatizabal@databi.com.co>
2020-03-23 22:32:08 -0300

    cliente rest Generador De Datos

--- desarrollo-desafio-uno/src/main/java/com/previred/periodos/desafio/DesarrolloDesafioUnoApplication.java
+++ desarrollo-desafio-uno/src/main/java/com/previred/periodos/desafio/DesarrolloDesafioUnoApplication.java
@@ -2,12 +2,20 @@ package com.previred.periodos.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DesarrolloDesafioUnoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesarrolloDesafioUnoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
