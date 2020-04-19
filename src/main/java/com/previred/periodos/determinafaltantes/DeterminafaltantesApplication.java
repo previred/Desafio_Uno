4a56bd870835c7274a78079e02963135c54391d2
Jose Yustiz <jose.yustiz@globallogic.com>
2020-04-19 12:01:16 -0400

    Proyecto inicial

---
+++ src/main/java/com/previred/periodos/determinafaltantes/DeterminafaltantesApplication.java
@@ -0,0 +1,13 @@
+package com.previred.periodos.determinafaltantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeterminafaltantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeterminafaltantesApplication.class, args);
+	}
+
+}
