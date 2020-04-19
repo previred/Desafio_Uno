c5b0866ddbb6cb3a4035215da1f61f1e6eed249a
Jose Yustiz <jose.yustiz@globallogic.com>
2020-04-19 13:22:25 -0400

    Refactorizacion de ApiPeriodoAdapterSpec

--- src/test/groovy/com/previred/periodos/determinafaltantes/infrastructure/ApiPeriodoAdapterSpec.groovy
+++ src/test/groovy/com/previred/periodos/determinafaltantes/infrastructure/ApiPeriodoAdapterSpec.groovy
@@ -3,17 +3,29 @@ package com.previred.periodos.determinafaltantes.infrastructure
 import com.previred.periodos.determinafaltantes.core.Periodo
 import com.previred.periodos.determinafaltantes.core.PeriodoPort
 import org.springframework.web.client.RestTemplate
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Subject
 
 class ApiPeriodoAdapterSpec extends Specification{
-    def"retorna un objeto Periodo vacio en caso que la consulta de la API Periodo retorne null"(){
-        given:
-        RestTemplate restTemplate = Stub(RestTemplate)
-        restTemplate.getForObject(_ as String, Periodo.class) >> null
-        def config = Stub(ApiPeriodoAdapterConfiguration)
+    @Shared private ApiPeriodoAdapterConfiguration config
+    @Shared private RestTemplate restTemplate
+    @Shared @Subject private  PeriodoPort adapter
+
+    void setupSpec() {
+        config = Stub(ApiPeriodoAdapterConfiguration)
         config.getUrl() >> new URL("http://localhost:8080/periodos/api")
         config.getFormatoFecha() >> "yyyy-MM-dd"
-        PeriodoPort adapter = new ApiPeriodoAdapter(restTemplate, config)
+    }
+
+    void setup() {
+        restTemplate = Stub(RestTemplate)
+        adapter = new ApiPeriodoAdapter(restTemplate, config)
+    }
+
+    def"retorna un objeto Periodo vacio en caso que la consulta de la API Periodo retorne null"(){
+        given:
+        restTemplate.getForObject(_ as String, Periodo.class) >> null
 
         when:
         Periodo periodo = adapter.getFechasPeriodoAleatorio()
@@ -26,10 +38,6 @@ class ApiPeriodoAdapterSpec extends Specification{
         given:
         RestTemplate restTemplate = Stub(RestTemplate)
         restTemplate.getForObject(_ as String, Periodo.class) >> new Exception("EXCEPTION")
-        def config = Stub(ApiPeriodoAdapterConfiguration)
-        config.getUrl() >> new URL("http://localhost:8080/periodos/api")
-        config.getFormatoFecha() >> "yyyy-MM-dd"
-        PeriodoPort adapter = new ApiPeriodoAdapter(restTemplate, config)
 
         when:
         Periodo periodo = adapter.getFechasPeriodoAleatorio()
