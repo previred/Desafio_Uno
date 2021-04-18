23eb723f12fca307cd23e9e3ba117a333a8eee0e
jorgesanmartinjeldres <jorgesanmartinjeldres@gmail.com>
2021-04-18 12:37:49 -0400

    Se crea metodo para determinar los periodos perdidos

--- Nivel-3/ApiPeriodosPerdidos/src/main/java/com/previred/periodosperdidos/servicio/PeriodosPerdidosService.java
+++ Nivel-3/ApiPeriodosPerdidos/src/main/java/com/previred/periodosperdidos/servicio/PeriodosPerdidosService.java
@@ -1,5 +1,14 @@
 package com.previred.periodosperdidos.servicio;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +22,22 @@ public class PeriodosPerdidosService {
 	private IPeriodosClient client;
 	
 	public Periodo obtenerPeriodosPerdidos() {
-		return client.periodos();
+		Periodo periodoGdd = client.periodos();
+		periodoGdd.setFechasFaltantes(determinarPeriodosPerdidos(periodoGdd));
+		return periodoGdd;
 	}
 	
-	
+	private List<LocalDate> determinarPeriodosPerdidos(Periodo p){
+		Map<LocalDate,LocalDate> fechasGeneradasMap = p.getFechas().stream().collect(Collectors.toMap(Function.identity(), Function.identity()));
+		List<LocalDate> resp = new ArrayList<LocalDate>();
+		for (LocalDate aux = p.getFechaCreacion(); aux.compareTo(p.getFechaFin()) <= 0; aux = aux.plus(1, ChronoUnit.MONTHS)) {
+			if (!fechasGeneradasMap.containsKey(aux)){
+				resp.add(aux);
+			}
+		}
+		return resp;
+		
+	}
 	
 
 }
