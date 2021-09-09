9880f968524e4291fa030afcf37988575ef8a931
Cristian Calabrán Veas <cristian.calabran@ionix.cl>
2021-09-09 18:14:17 -0300

    Versión inicial

--- src/main/java/cl/previred/gdd/controllers/GddController.java
+++ src/main/java/cl/previred/gdd/controllers/GddController.java
@@ -4,12 +4,6 @@ import cl.previred.gdd.dto.GddRequest;
 import cl.previred.gdd.dto.GddResponse;
 import cl.previred.gdd.services.IGddService;
 import cl.previred.gdd.services.ServiceException;
-
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Example;
-import io.swagger.annotations.ExampleProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,14 +19,7 @@ public class GddController {
   @Autowired
   private IGddService gddService;
 
-  @ApiOperation(value = "Servicio que calcula las fechas faltantes en un período determinado.", notes = "Servicio que calcula las fechas faltantes en un período determinado")
-  @ApiResponses(value = {
-          @ApiResponse(code = 200, message = "OK", examples =
-                  @Example(value = {@ExampleProperty(mediaType = "application/json", value = "{\"id\": 6, \"fechaCreacion\": \"1969-03-01\", \"fechaFin\": \"1970-01-01\", \"fechas\": [\"1969-03-01\", \"1969-05-01\", \"1969-09-01\", \"1970-01-01\"]}")})
-          )
-  })
   @PostMapping(path = "/rest/gdd")
-
   public ResponseEntity<GddResponse> gdd(@RequestBody GddRequest request) throws ServiceException {
     GddResponse gddResponse = gddService.calculateMissingPeriods(request);
     return ResponseEntity.ok(gddResponse);
--- src/main/java/cl/previred/gdd/dto/GddGenericDto.java
+++ src/main/java/cl/previred/gdd/dto/GddGenericDto.java
@@ -1,15 +1,9 @@
 package cl.previred.gdd.dto;
 
-import io.swagger.annotations.ApiModelProperty;
-
 public abstract class GddGenericDto {
 
   private Integer id;
-
-  @ApiModelProperty(notes = "Este campo es una fecha en formato YYYY-MM-dd que indica el inicio del período a calcular", example = "\"2017-12-30\"")
   private String fechaCreacion;
-
-  @ApiModelProperty(notes = "Este campo es una fecha en formato YYYY-MM-dd que indica el fin del período a calcular", example = "\"2020-01-15\"")
   private String fechaFin;
 
   public Integer getId() {
