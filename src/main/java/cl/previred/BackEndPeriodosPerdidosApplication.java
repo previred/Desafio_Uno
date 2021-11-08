237e84a04e6acb13708f02cdea04d1de1e8b1e72
ANIBAL DAVID ABELLO HERNANDEZ <anibal.abello@coopeuch.cl>
2021-11-08 16:56:47 -0300

    pruebas 1

---
+++ src/main/java/cl/previred/BackEndPeriodosPerdidosApplication.java
@@ -0,0 +1,13 @@
+package cl.previred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPeriodosPerdidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPeriodosPerdidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/previred/BackEndPeriodosPerdidosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.previred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPeriodosPerdidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
